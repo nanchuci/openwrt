f61ca5692c0635ab302429ecd94cc664b46128c8
nanchuci <71806013+nanchuci@users.noreply.github.com>
2021-03-02 14:24:56 +0800

    add e8820s

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -1510,6 +1510,7 @@ TARGET_DEVICES += zyxel_wap6805
 
 define Device/zte_e8820s
   $(Device/dsa-migration)
+  $(Device/uimage-lzma-loader)
   BLOCKSIZE := 128k
   PAGESIZE := 2048
   KERNEL_SIZE := 4096k
